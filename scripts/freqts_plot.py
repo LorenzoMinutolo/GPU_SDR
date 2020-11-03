b73f43a754933c1304d613505e3e6f6fc13f3981
Lorenzo <minutolo@caltech.edu>
2020-11-03 08:44:32 -0800

    Added initial N321 support

--- scripts/freqts_plot.py
+++ scripts/freqts_plot.py
@@ -11,9 +11,9 @@ except ImportError:
         print("Cannot find the pyUSRP package")
 
 import argparse
-def run(backend, files, decimation, low_pass, channel_list, displayed_samples):
+def run(backend, files, decimation, low_pass, channel_list, displayed_samples, start_time, end_time):
     u.plot_frequency_timestreams(files, decimation=decimation, low_pass=20, backend=backend, output_filename=None,
-              channel_list=ch_list, auto_open=True, displayed_samples = displayed_samples)
+              channel_list=ch_list, auto_open=True, displayed_samples = displayed_samples, start_time=start_time, end_time=end_time)
 
 
 if __name__ == "__main__":
@@ -26,6 +26,8 @@ if __name__ == "__main__":
     parser.add_argument('--displayed_samples', '-ds', help='Total number of samples per channel to plot. override decimation', type=int)
     parser.add_argument('--channel_list', '-ch', help='Channel number to plot, single value', type=int)
     parser.add_argument('--lowpass', '-lp', help='Low pass filter order', type=int)
+    parser.add_argument('--start', '-s', help='Start time for plotting', type=int, default = None)
+    parser.add_argument('--end', '-e', help='End time for plotting', type=int, default = None)
 
 
     args = parser.parse_args()
@@ -44,4 +46,4 @@ if __name__ == "__main__":
 
     files = glob.glob("USRP_Noise*.h5")
 
-    run(backend = args.backend, files = files, decimation = args.decimation, displayed_samples = args.displayed_samples, channel_list = ch_list, low_pass = args.lowpass)
+    run(backend = args.backend, files = files, decimation = args.decimation, displayed_samples = args.displayed_samples, channel_list = ch_list, low_pass = args.lowpass, start_time = args.start, end_time = args.end)
